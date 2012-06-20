7f5c68ab4907315c15ac8cce10cb8b8f4d6d7833
Michael Schneider <michael.schneider@adtegrity.com>
2012-06-20 12:16:13 -0400

    Removed web socket demo

--- web.go
+++ web.go
@@ -1,193 +1,193 @@
 package ym
 
-import (
-	// "fmt"
-	"net/http"
-  "flag"
-  "html/template"
-  "os"
-  "code.google.com/p/go.net/websocket"
-)
+// import (
+// 	// "fmt"
+// 	"net/http"
+// 	"flag"
+// 	"html/template"
+// 	"os"
+// 	"code.google.com/p/go.net/websocket"
+// )
 
-// func handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-// }
+// // func handler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+// // }
+// // func main() {
+// 	// http.HandleFunc("/", handler)
+// 	// http.ListenAndServe(":8080", nil)
+// // }
+
+// var addr = flag.String("addr", ":8080", "http service address")
+
 // func main() {
-	// http.HandleFunc("/", handler)
-	// http.ListenAndServe(":8080", nil)
+//   flag.Parse()
+//   go hub()
+//   http.HandleFunc("/", homeHandler)
+//   http.HandleFunc("/ws", webSocketProtocolSwitch)
+//   println("Server Listening on port 8080")
+//   if err := http.ListenAndServe(*addr, nil); err != nil {
+//     println("ListenAndServe: ", err);
+//     os.Exit(-1)
+//   }
 // }
 
-var addr = flag.String("addr", ":8080", "http service address")
+// func webSocketProtocolSwitch(c http.ResponseWriter, req *http.Request) {
+//   // Handle old and new versions of protocol.
+//   if _, found := req.Header["Sec-Websocket-Key1"]; found {
+//     websocket.Handler(clientHandler).ServeHTTP(c, req)
+//   } else {
+//     websocket.Handler(clientHandler).ServeHTTP(c, req)
+//   }
+// }
 
-func main() {
-  flag.Parse()
-  go hub()
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/ws", webSocketProtocolSwitch)
-  println("Server Listening on port 8080")
-  if err := http.ListenAndServe(*addr, nil); err != nil {
-    println("ListenAndServe: ", err);
-    os.Exit(-1)
-  }
-}
+// var messageChan = make(chan []byte)
 
-func webSocketProtocolSwitch(c http.ResponseWriter, req *http.Request) {
-  // Handle old and new versions of protocol.
-  if _, found := req.Header["Sec-Websocket-Key1"]; found {
-    websocket.Handler(clientHandler).ServeHTTP(c, req)
-  } else {
-    websocket.Handler(clientHandler).ServeHTTP(c, req)
-  }
-}
+// type subscription struct {
+//   conn      *websocket.Conn
+//   subscribe bool
+// }
 
-var messageChan = make(chan []byte)
+// var subscriptionChan = make(chan subscription)
 
-type subscription struct {
-  conn      *websocket.Conn
-  subscribe bool
-}
+// func hub() {
+//   conns := make(map[*websocket.Conn]int)
+//   for {
+//     select {
+//     case subscription := <-subscriptionChan:
+//       conns[subscription.conn] = 0 //, subscription.subscribe
+//     case message := <-messageChan:
+//       for conn, _ := range conns {
+//         if _, err := conn.Write(message); err != nil {
+//           conn.Close()
+//         }
+//       }
+//     }
+//   }
+// }
 
-var subscriptionChan = make(chan subscription)
+// func clientHandler(ws *websocket.Conn) {
+//   defer func() {
+//     subscriptionChan <- subscription{ws, false}
+//     ws.Close()
+//   }()
 
-func hub() {
-  conns := make(map[*websocket.Conn]int)
-  for {
-    select {
-    case subscription := <-subscriptionChan:
-      conns[subscription.conn] = 0 //, subscription.subscribe
-    case message := <-messageChan:
-      for conn, _ := range conns {
-        if _, err := conn.Write(message); err != nil {
-          conn.Close()
-        }
-      }
-    }
-  }
-}
+//   subscriptionChan <- subscription{ws, true}
 
-func clientHandler(ws *websocket.Conn) {
-  defer func() {
-    subscriptionChan <- subscription{ws, false}
-    ws.Close()
-  }()
+//   buf := make([]byte, 256)
+//   for {
+//     n, err := ws.Read(buf)
+//     if err != nil {
+//       break
+//     }
+//     messageChan <- buf[0:n]
+//   }
+// }
 
-  subscriptionChan <- subscription{ws, true}
+// // Handle home page requests.
+// func homeHandler(c http.ResponseWriter, req *http.Request) {
+//   homeTempl.Execute(c, req.Host)
+// }
 
-  buf := make([]byte, 256)
-  for {
-    n, err := ws.Read(buf)
-    if err != nil {
-      break
-    }
-    messageChan <- buf[0:n]
-  }
-}
+// var homeTempl *template.Template
 
-// Handle home page requests.
-func homeHandler(c http.ResponseWriter, req *http.Request) {
-  homeTempl.Execute(c, req.Host)
-}
+// func init() {
+//   homeTempl = template.New("")
+//   homeTempl.Delims("<<", ">>")
+//   if _, err := homeTempl.Parse(homeStr); err != nil {
+//     println("template error: ", err.Error())
+//     panic("")
+//   }
+// }
 
-var homeTempl *template.Template
+// const homeStr = `
+// <html>
+// <head>
+// <title>Chat Example</title>
+// <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+// <script type="text/javascript">
+//     $(function() {
 
-func init() {
-  homeTempl = template.New("")
-  homeTempl.Delims("<<", ">>")
-  if _, err := homeTempl.Parse(homeStr); err != nil {
-    println("template error: ", err.Error())
-    panic("")
-  }
-}
+//     var conn;
+//     var msg = $("#msg");
+//     var log = $("#log");
 
-const homeStr = `
-<html>
-<head>
-<title>Chat Example</title>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript">
-    $(function() {
+//     function appendLog(msg) {
+//         var d = log[0]
+//         var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
+//         msg.appendTo(log)
+//         if (doScroll) {
+//             d.scrollTop = d.scrollHeight - d.clientHeight;
+//         }
+//     }
 
-    var conn;
-    var msg = $("#msg");
-    var log = $("#log");
+//     $("#form").submit(function() {
+//         if (!conn) {
+//             return false;
+//         }
+//         if (!msg.val()) {
+//             return false;
+//         }
+//         conn.send(msg.val());
+//         msg.val("");
+//         return false
+//     });
 
-    function appendLog(msg) {
-        var d = log[0]
-        var doScroll = d.scrollTop == d.scrollHeight - d.clientHeight;
-        msg.appendTo(log)
-        if (doScroll) {
-            d.scrollTop = d.scrollHeight - d.clientHeight;
-        }
-    }
+//     if (window["WebSocket"]) {
+//         conn = new WebSocket("ws://<<.>>/ws");
+//         conn.onclose = function(evt) {
+//             appendLog($("<div><b>Connection closed.</b></div>"))
+//         }
+//         conn.onmessage = function(evt) {
+//             appendLog($("<div/>").text(evt.data))
+//         }
+//     } else {
+//         appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
+//     }
+//     });
+// </script>
+// <style type="text/css">
+// html {
+//     overflow: hidden;
+// }
 
-    $("#form").submit(function() {
-        if (!conn) {
-            return false;
-        }
-        if (!msg.val()) {
-            return false;
-        }
-        conn.send(msg.val());
-        msg.val("");
-        return false
-    });
+// body {
+//     overflow: hidden;
+//     padding: 0;
+//     margin: 0;
+//     width: 100%;
+//     height: 100%;
+//     background: gray;
+// }
 
-    if (window["WebSocket"]) {
-        conn = new WebSocket("ws://<<.>>/ws");
-        conn.onclose = function(evt) {
-            appendLog($("<div><b>Connection closed.</b></div>"))
-        }
-        conn.onmessage = function(evt) {
-            appendLog($("<div/>").text(evt.data))
-        }
-    } else {
-        appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
-    }
-    });
-</script>
-<style type="text/css">
-html {
-    overflow: hidden;
-}
+// #log {
+//     background: white;
+//     margin: 0;
+//     padding: 0.5em 0.5em 0.5em 0.5em;
+//     position: absolute;
+//     top: 0.5em;
+//     left: 0.5em;
+//     right: 0.5em;
+//     bottom: 3em;
+//     overflow: auto;
+// }
 
-body {
-    overflow: hidden;
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    height: 100%;
-    background: gray;
-}
+// #form {
+//     padding: 0 0.5em 0 0.5em;
+//     margin: 0;
+//     position: absolute;
+//     bottom: 1em;
+//     left: 0px;
+//     width: 100%;
+//     overflow: hidden;
+// }
 
-#log {
-    background: white;
-    margin: 0;
-    padding: 0.5em 0.5em 0.5em 0.5em;
-    position: absolute;
-    top: 0.5em;
-    left: 0.5em;
-    right: 0.5em;
-    bottom: 3em;
-    overflow: auto;
-}
-
-#form {
-    padding: 0 0.5em 0 0.5em;
-    margin: 0;
-    position: absolute;
-    bottom: 1em;
-    left: 0px;
-    width: 100%;
-    overflow: hidden;
-}
-
-</style>
-</head>
-<body>
-<div id="log"></div>
-<form id="form">
-    <input type="submit" value="Send" />
-    <input type="text" id="msg" size="64"/>
-</form>
-</body>
-</html> `
+// </style>
+// </head>
+// <body>
+// <div id="log"></div>
+// <form id="form">
+//     <input type="submit" value="Send" />
+//     <input type="text" id="msg" size="64"/>
+// </form>
+// </body>
+// </html> `
