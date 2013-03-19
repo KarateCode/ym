1dcc80531dbb648b6475f7cf01f771a13ae67895
Michael Schneider <michael.schneider@adtegrity.com>
2013-03-19 11:38:17 -0400

    Retries more times and waits longer between

--- ym.go
+++ ym.go
@@ -1,18 +1,18 @@
 package ym
 
 import (
+	"bytes"
+	"encoding/xml"
+	"errors"
 	"fmt"
 	"html"
-	"encoding/xml"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"text/template"
-	"bytes"
-	"io"
 	"os"
-	"errors"
-	"time"
 	"strings"
+	"text/template"
+	"time"
 )
 
 var token string
@@ -23,7 +23,7 @@ var reportRoot string
 var version = "1.35"
 
 type Auth struct {
-  Login, Password, Env, url string
+	Login, Password, Env, url string
 }
 
 type Column []string
@@ -45,7 +45,9 @@ var layoutTemplate *template.Template
 func init() {
 	var err error
 	layoutTemplate, err = template.New("Layout").Parse(layoutXml)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	templatePath = strings.Split(os.Getenv("GOPATH"), ":")[0] + "/src/github.com/KarateCode/ym/"
 }
 
@@ -57,7 +59,7 @@ func Session(cred Auth, work func()) {
 
 func Open(cred Auth) error {
 	credentials = cred
-  
+
 	if credentials.Env == "test" {
 		credentials.url = "https://api-test.yieldmanager.com/api-" + version + "/"
 		reportRoot = "https://api-test.yieldmanager.com/reports/"
@@ -65,42 +67,54 @@ func Open(cred Auth) error {
 		credentials.url = "https://api.yieldmanager.com/api-" + version + "/"
 		reportRoot = "https://api.yieldmanager.com/reports/"
 	}
-  
+
 	tmpl, err := template.ParseFiles(templatePath + "templates/contact/login.xml")
-	if err != nil { return err }
-	
+	if err != nil {
+		return err
+	}
+
 	buffer := new(bytes.Buffer)
 	err = tmpl.Execute(buffer, credentials)
-	if err != nil { return err }
-	
-	req, err := http.NewRequest("POST", credentials.url + "contact.php", buffer)
-	if err != nil { return err }
-	
-	res, error := http.DefaultClient.Do(req)  
-	if error != nil { return error }
-    
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", credentials.url+"contact.php", buffer)
+	if err != nil {
+		return err
+	}
+
+	res, error := http.DefaultClient.Do(req)
+	if error != nil {
+		return error
+	}
+
 	type loginStruct struct {
 		XMLName xml.Name
-		Body struct {
-			XMLName xml.Name
+		Body    struct {
+			XMLName  xml.Name
 			Innerxml string "innerxml"
-			Fault struct {
-				XMLName xml.Name `xml:"Fault"`
-				Faultstring string `xml:"faultstring"`
-			} 
+			Fault    struct {
+				XMLName     xml.Name `xml:"Fault"`
+				Faultstring string   `xml:"faultstring"`
+			}
 			LoginResponse struct {
 				XMLName xml.Name `xml:"loginResponse"`
-				Token string `xml:"token"`
+				Token   string   `xml:"token"`
 			}
 		}
 	}
 	loginObj := new(loginStruct)
 	p, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil { return readErr }
-	
+	if readErr != nil {
+		return readErr
+	}
+
 	error = xml.Unmarshal(p, loginObj)
-	if error != nil { return error }
-    
+	if error != nil {
+		return error
+	}
+
 	token = loginObj.Body.LoginResponse.Token
 	if Verbose {
 		println("\n** Logged in:", credentials.url, " **")
@@ -109,21 +123,27 @@ func Open(cred Auth) error {
 	if len(loginObj.Body.Fault.Faultstring) > 0 {
 		return errors.New(loginObj.Body.Fault.Faultstring)
 	}
-	
+
 	return nil
 }
 
 func AssembleTemplate(bodyXml string, data interface{}) *bytes.Buffer {
 	tmpl, err := layoutTemplate.Clone()
-	if err != nil { panic(err) }
-	
+	if err != nil {
+		panic(err)
+	}
+
 	_, err = tmpl.Parse(bodyXml)
-	if err != nil { panic(err) }
-	
+	if err != nil {
+		panic(err)
+	}
+
 	buffer := new(bytes.Buffer)
 	err = tmpl.Execute(buffer, data)
-	if err != nil { panic(err) }
-	
+	if err != nil {
+		panic(err)
+	}
+
 	return buffer
 }
 
@@ -135,21 +155,21 @@ func checkConnection() {
 
 func ComplexReport(requestXml string) (*ReportData, *Row, error) {
 	checkConnection()
-	
+
 	type Data struct {
-		Token string
+		Token     string
 		XmlString string
 	}
-	
+
 	bodyXml := `{{define "Body"}}<n1:requestViaXML env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:n1="urn:ReportService">
 		<token xsi:type="xsd:string">{{.Token}}</token>
 		<xml xsi:type="xsd:string">{{.XmlString}}</xml>
 		</n1:requestViaXML>{{end}}`
 
-	buffer := AssembleTemplate(bodyXml, Data{Token:token, XmlString:html.EscapeString(requestXml)})
+	buffer := AssembleTemplate(bodyXml, Data{Token: token, XmlString: html.EscapeString(requestXml)})
 	// io.Copy(os.Stdout, buffer)
 	// println(credentials.url + "report.php")
-	req, err := http.NewRequest("POST", credentials.url + "report.php", buffer)
+	req, err := http.NewRequest("POST", credentials.url+"report.php", buffer)
 	if err != nil {
 		panic(err)
 	}
@@ -157,18 +177,18 @@ func ComplexReport(requestXml string) (*ReportData, *Row, error) {
 	if error != nil {
 		panic(error)
 	}
-	
+
 	// io.Copy(os.Stdout, res.Body)
-	
+
 	type RequestViaXml struct {
 		XMLName xml.Name
-		Body struct {
+		Body    struct {
 			XMLName  xml.Name
 			Innerxml string "innerxml"
-			Fault struct {
+			Fault    struct {
 				XMLName     xml.Name `xml:"Fault"`
 				Faultstring string   `xml:"faultstring"`
-			} 
+			}
 			RequestViaXMLResponse struct {
 				XMLName     xml.Name `xml:"requestViaXMLResponse"`
 				Token       string   `xml:"token"`
@@ -178,11 +198,11 @@ func ComplexReport(requestXml string) (*ReportData, *Row, error) {
 	}
 	requestViaXml := new(RequestViaXml)
 	p, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil { 
+	if readErr != nil {
 		panic(readErr)
 		// return nil, readErr 
 	}
-	
+
 	errUnmarshall := xml.Unmarshal(p, requestViaXml)
 	// if error != nil { return nil, readErr }
 	if errUnmarshall != nil {
@@ -192,11 +212,10 @@ func ComplexReport(requestXml string) (*ReportData, *Row, error) {
 		return nil, nil, errors.New(requestViaXml.Body.Fault.Faultstring)
 	}
 
-
 	// println(requestViaXml.Body.RequestViaXMLResponse.ReportToken)
 	retries := 0
 	var reportUrl string
-	for retries < 6 {
+	for retries < 20 {
 		reportUrl, err = Status(requestViaXml.Body.RequestViaXMLResponse.ReportToken)
 		if err != nil {
 			panic(err)
@@ -206,50 +225,50 @@ func ComplexReport(requestXml string) (*ReportData, *Row, error) {
 			break
 		}
 		println("reportUrl:", reportUrl)
-		println("sleeping")
-		time.Sleep(15 * time.Second)
-		println("reattempting")
+		println("sleeping ", retries)
+		time.Sleep(30 * time.Second)
+		println("reattempting ", requestViaXml.Body.RequestViaXMLResponse.ReportToken)
 		retries += 1
 	}
-	
+
 	downloadReq, downloadErr := http.NewRequest("GET", reportUrl, nil)
-	if downloadErr != nil { 
+	if downloadErr != nil {
 		// return nil, downloadErr 
 		panic(downloadErr)
 	}
-		
+
 	type IoData struct {
 		Response struct {
 			XMLName xml.Name `xml:"RESPONSE"`
-			Data struct {
+			Data    struct {
 				XMLName xml.Name `xml:"DATA"`
 
-				Header Row `xml:"HEADER"`
-				RData ReportData `xml:"ROW"`
+				Header Row        `xml:"HEADER"`
+				RData  ReportData `xml:"ROW"`
 			}
 		}
 	}
-	
+
 	retries = 0
 	ioData := new(IoData)
 	for retries < 6 {
-		downloadRes, errGet := http.DefaultClient.Do(downloadReq)  
+		downloadRes, errGet := http.DefaultClient.Do(downloadReq)
 		if errGet != nil {
 			panic(errGet)
 		}
 		defer downloadRes.Body.Close()
-		
+
 		// io.Copy(os.Stdout, downloadRes.Body)
 		// return nil, nil
-		
+
 		p, readErr = ioutil.ReadAll(downloadRes.Body)
-		if readErr != nil { 
+		if readErr != nil {
 			// return nil, readErr 
 			panic(readErr)
 		}
-		
+
 		errUnmarshall = xml.Unmarshal(p, ioData)
-		if errUnmarshall != nil { 
+		if errUnmarshall != nil {
 			// return nil, errUnmarshall 
 			println("\n", reportUrl, "\n")
 			println(string(p))
@@ -265,61 +284,65 @@ func ComplexReport(requestXml string) (*ReportData, *Row, error) {
 			break
 		}
 	}
-	
-	// fmt.Printf("Header: %+v\n", ioData.Response.Data.Header)
+
+	fmt.Printf("Header: %+v\n", ioData.Response.Data)
 	return &ioData.Response.Data.RData, &ioData.Response.Data.Header, nil
 }
 
 func Status(reportToken string) (string, error) {
 	type StatusData struct {
-		Token string
+		Token       string
 		ReportToken string
 	}
-	
+
 	statusXml := `{{define "Body"}}<n1:status env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:n1="urn:ReportService">
 		<token xsi:type="xsd:string">{{.Token}}</token>
 		<report_token xsi:type="xsd:string">{{.ReportToken}}</report_token>
 		</n1:status>{{end}}`
-	
-	buffer := AssembleTemplate(statusXml, StatusData{Token:token, ReportToken:reportToken})
+
+	buffer := AssembleTemplate(statusXml, StatusData{Token: token, ReportToken: reportToken})
 	// io.Copy(os.Stdout, buffer)  
-	req, err := http.NewRequest("POST", credentials.url + "report.php", buffer)
+	req, err := http.NewRequest("POST", credentials.url+"report.php", buffer)
 	if err != nil {
 		println("error creating request")
 		panic(err)
 	}
-	res, error := http.DefaultClient.Do(req)  
+	res, error := http.DefaultClient.Do(req)
 	if error != nil {
 		println("error posting adhoc report")
 		panic(error)
 	}
-	
+
 	// io.Copy(os.Stdout, res.Body)  
 	// return "", nil
-	
+
 	type Status struct {
 		XMLName xml.Name
-		Body struct {
-			XMLName xml.Name
+		Body    struct {
+			XMLName  xml.Name
 			Innerxml string "innerxml"
-			Fault struct {
-				XMLName xml.Name `xml:"Fault"`
-				Faultstring string `xml:"faultstring"`
-			} 
+			Fault    struct {
+				XMLName     xml.Name `xml:"Fault"`
+				Faultstring string   `xml:"faultstring"`
+			}
 			StatusResponse struct {
-				XMLName xml.Name `xml:"statusResponse"`
-				Token string `xml:"token"`
-				UrlReport string `xml:"url_report"`
+				XMLName   xml.Name `xml:"statusResponse"`
+				Token     string   `xml:"token"`
+				UrlReport string   `xml:"url_report"`
 			}
 		}
 	}
 	status := new(Status)
 	p, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil { return "", readErr }
-	
+	if readErr != nil {
+		return "", readErr
+	}
+
 	error = xml.Unmarshal(p, status)
-	if error != nil { return "", error }
-	
+	if error != nil {
+		return "", error
+	}
+
 	if len(status.Body.Fault.Faultstring) > 0 {
 		return "", errors.New(status.Body.Fault.Faultstring)
 	}
@@ -330,29 +353,37 @@ func Status(reportToken string) (string, error) {
 func Close() {
 	ManualClose(token)
 }
-  
+
 func ManualClose(manualToken string) {
 	if credentials.Env == "test" {
 		credentials.url = "https://api-test.yieldmanager.com/api-" + version + "/"
 	} else {
 		credentials.url = "https://api.yieldmanager.com/api-" + version + "/"
 	}
-	
+
 	tmpl, err := template.ParseFiles(templatePath + "templates/contact/logout.xml")
-	if err != nil { panic(err) }
-	
+	if err != nil {
+		panic(err)
+	}
+
 	buffer := new(bytes.Buffer)
 	err = tmpl.Execute(buffer, manualToken)
-	if err != nil { panic(err) }
-	
-	req, reqErr := http.NewRequest("POST", credentials.url + "contact.php", buffer)
-	if reqErr != nil { panic(reqErr) }
-	
-	res, resErr := http.DefaultClient.Do(req)  
-	if resErr != nil { panic(resErr)  }
+	if err != nil {
+		panic(err)
+	}
+
+	req, reqErr := http.NewRequest("POST", credentials.url+"contact.php", buffer)
+	if reqErr != nil {
+		panic(reqErr)
+	}
+
+	res, resErr := http.DefaultClient.Do(req)
+	if resErr != nil {
+		panic(resErr)
+	}
 	if Verbose {
 		println("\n** Logged out **")
-		io.Copy(os.Stdout, res.Body)  
+		io.Copy(os.Stdout, res.Body)
 	}
 	token = ""
 }
